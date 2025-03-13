a2541d3b1f055bd3a0de399f9fb422f1a43bdee5
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-03-13 22:41:38 +0000

    Make functions to check general win conditions
    Make functions to check all possible win conditions: horizontal,
    vertical, and diagonal.

--- script.js
+++ script.js
@@ -17,3 +17,79 @@ function displayBoard(gameboard) {
         console.log(row);
     }
 }
+
+function colChecker(gameBoard) {
+    /* Check if any column has the same tokens in each cell */
+
+    let numCols = gameBoard[0].length;
+    for (let col = 0; col < numCols; col++) {
+        // value of element in first row of column
+        const topColValue = gameBoard[0][col];
+        let matchCounter = 0;
+        // go through rest of the rows
+        for (let row = 1; row < gameBoard.length; row++) {
+            if (topColValue !== gameBoard[row][col]) {
+                break;
+            }
+            matchCounter++;
+            if (matchCounter === (gameBoard.length - 1)) {
+                return topColValue;
+            }
+        }
+    }
+    return null;
+}
+
+function rowChecker(gameBoard) {
+    /* Check if any row has the same tokens in each cell */
+
+    for (let row = 0; row < gameBoard.length; row++) {
+        // value of element in first column of row
+        const firstRowVal = gameBoard[row][0];
+        let matchCounter = 0;
+        // go through rest of the columns in the row
+        for (let col = 1; col < gameBoard[0].length; col++) {
+            if (firstRowVal !== gameBoard[row][col]) {
+                break;
+            }
+            matchCounter++;
+            if (matchCounter === (gameBoard[0].length - 1)) {
+                return firstRowVal;
+            }
+        }
+    }
+    return null;
+}
+
+function diagonalChecker(gameBoard) {
+    /* Check if any diagonal has the same tokens in each cell */
+
+    const size = gameBoard.length;
+
+    // Main diagonal
+    const topLeftCornerVal = gameBoard[0][0];
+    let diagMatchCount = 0;
+    for (let row = 1; row < size; row++) {
+        if (topLeftCornerVal !== gameBoard[row][row]) {
+            break;
+        }
+        diagMatchCount++;
+        if (diagMatchCount === (size - 1)) {
+            return topLeftCornerVal;
+        }
+    }
+
+    // Anti-diagonal
+    const topRightCornerVal = gameBoard[0][size - 1];
+    let antiMatchCount = 0;
+    for (let row = 1; row < size; row++) {
+        if (topRightCornerVal !== gameBoard[row][size - 1 - row]) {
+            break;
+        }
+        antiMatchCount++;
+        if (antiMatchCount === size - 1) {
+            return topRightCornerVal;
+        }
+    }
+    return null;
+}
